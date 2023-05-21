bd9b46cdea47d2e5bffba6e6338d6b927c51c2ca
ELOISE BROCHIER <eloise.brochier@gmail.com>
2023-05-21 15:25:45 +0200

    quest 5

--- app.js
+++ app.js
@@ -19,8 +19,9 @@ const movieHandlers = require("./movieHandlers");
 
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
-app.post("/api/movies", movieHandlers.postMovie)
-app.put("/api/movies/:id", movieHandlers.updateMovie)
+app.post("/api/movies", movieHandlers.postMovie);
+app.put("/api/movies/:id", movieHandlers.updateMovie);
+app.delete("/api/movies/:id", movieHandlers.deleteMovie);
 
 
 //USERS 
@@ -28,8 +29,9 @@ const userHandlers = require("./userHandlers");
 
 app.get("/api/users", userHandlers.getUsers);
 app.get("/api/users/:id", userHandlers.getUserById);
-app.post("/api/users", userHandlers.postUser)
-app.put("/api/users/:id", userHandlers.updateUser)
+app.post("/api/users", userHandlers.postUser);
+app.put("/api/users/:id", userHandlers.updateUser);
+app.delete("/api/users/:id", userHandlers.deleteUser);
 
 
 app.listen(port, (err) => {
